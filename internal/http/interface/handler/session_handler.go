8b6cd30889ec79aef3ff43e6cecb7f1055127020
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-03-08 12:06:01 +0900

    [mod]disabled path parameter

--- internal/http/interface/handler/session_handler.go
+++ internal/http/interface/handler/session_handler.go
@@ -3,7 +3,9 @@ package handler
 import (
 	"context"
 	"examples/internal/http/logic"
+	"examples/message"
 	"net/http"
+	"strings"
 )
 
 type sessionHandler struct {
@@ -17,25 +19,35 @@ func NewSessionHandler(userLogic logic.UserLogic) *sessionHandler {
 }
 
 func (h *sessionHandler) signin(ctx context.Context, w http.ResponseWriter, r *http.Request) *handleError {
-	if _, err := h.userLogic.Login(ctx, "admin", "admin"); err != nil {
+	if err := r.ParseForm(); err != nil {
+		return &handleError{msg: message.ErrParseForm.Error(), code: http.StatusBadRequest}
+	}
+
+	if _, err := h.userLogic.Signin(ctx, "admin", "admin"); err != nil {
 		return &handleError{msg: "login failed", code: http.StatusUnauthorized}
 	}
 	return nil
 }
 
-func (h *sessionHandler) signout(ctx context.Context) {
-	h.userLogic.Logout(ctx)
+func (h *sessionHandler) signout(ctx context.Context, w http.ResponseWriter, r *http.Request) *handleError {
+	h.userLogic.Signout(ctx)
+	return nil
 }
 
-func (h *sessionHandler) HandleRoot(w http.ResponseWriter, r *http.Request) (err *handleError) {
+func (h *sessionHandler) HandleRoot(w http.ResponseWriter, r *http.Request) *handleError {
+	path := strings.TrimPrefix(r.URL.Path, "session/")
+	if len(path) > 0 {
+		return ErrNotFound
+	}
+
 	ctx := r.Context()
 	switch r.Method {
 	case http.MethodPost:
-		err = h.signin(ctx, w, r)
+		return h.signin(ctx, w, r)
 	case http.MethodDelete:
-		h.signout(ctx)
+		return h.signout(ctx, w, r)
 	default:
 		http.NotFound(w, r)
 	}
-	return
+	return nil
 }
