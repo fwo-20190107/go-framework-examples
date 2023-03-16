dfa8633986ec575bd4d431530c81dbb73ae77a51
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-03-16 16:47:42 +0900

    [add]last signed

--- testdata/login.sql
+++ testdata/login.sql
@@ -1,14 +1,16 @@
 CREATE TABLE login (
     `login_id` TEXT NOT NULL PRIMARY KEY,
     `user_id` INTEGER NOT NULL,
+    `last_signed_at` TEXT,
     `password` TEXT NOT NULL
 );
 
 INSERT INTO login (
     `login_id`,
     `user_id`,
+    `last_signed_at`,
     `password`
 ) VALUES
-("admin", 1, "admin"),
-("user", 2, "user")
+("admin", 1, null, "admin"),
+("user", 2, null, "user")
 ;
