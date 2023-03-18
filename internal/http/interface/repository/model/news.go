f5c644524a4bb39186566d7cd81c7d92ce263838
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-03-18 21:27:06 +0900

    [del]stop responding to news

--- internal/http/interface/repository/model/news.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type News struct {
-	NewsID int    `db:"news_id"`
-	Title  string `db:"title"`
-	Body   string `db:"body"`
-}
--- internal/http/interface/repository/news_repository.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-type newsRepository struct {
-	SqlHandler
-}
-
-func NewNewsRepository(handler SqlHandler) *newsRepository {
-	return &newsRepository{handler}
-}
