ce717c5fa603e0b1d5c1527a2cf0328fd8e909fe
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-03-20 23:48:05 +0900

    [add]difinition common handle error

--- internal/http/interface/handler/errors.go
+++ internal/http/interface/handler/errors.go
@@ -4,6 +4,7 @@ import "examples/internal/http/interface/infra"
 
 var (
 	ErrUnexpected   = newErrorResponse("エラー", "不明なエラー")
+	ErrPathNotExist = newErrorResponse("アクセスエラー", "アクセスパスが存在しません")
 	ErrValidParam   = newErrorResponse("入力値エラー", "入力された値が不正です")
 	ErrFailedSignin = newErrorResponse("サインイン失敗", "ログインIDまたはパスワードが間違っています")
 )
