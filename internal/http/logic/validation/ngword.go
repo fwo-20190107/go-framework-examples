ed685f9e3a3e00cf4b7cc7f1b41b60f8042a7822
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2023-03-25 21:43:08 +0900

    [add]custom validation
    check to contain any prohibited words.

---
+++ internal/http/logic/validation/ngword.go
@@ -0,0 +1,41 @@
+package validation
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+
+	validation "github.com/go-ozzo/ozzo-validation"
+)
+
+var NgWord = &ngWord{message: "contains prohibited words"}
+
+type ngWord struct {
+	message string
+}
+
+func (v *ngWord) Validate(value interface{}) error {
+	isString, str, _, _ := validation.StringOrBytes(value)
+	if !isString {
+		return errors.New("value is not a comparable string")
+	}
+
+	for _, word := range ngWords {
+		if matched, err := regexp.MatchString(word, str); err != nil {
+			return err
+		} else if matched {
+			return fmt.Errorf(v.message+":%s", word)
+		}
+	}
+	return nil
+}
+
+func (v *ngWord) Error(message string) *ngWord {
+	return &ngWord{
+		message: message,
+	}
+}
+
+var ngWords = []string{
+	"hoge",
+}
