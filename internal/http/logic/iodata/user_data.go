ac569ea5e733e17ed07f24888ae624d343ea86ca
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2023-03-28 01:53:35 +0900

    [add]modify input and output

--- internal/http/logic/iodata/user_data.go
+++ internal/http/logic/iodata/user_data.go
@@ -13,12 +13,34 @@ type SignupInput struct {
 }
 
 func (i SignupInput) Validate() error {
-	err := validation.ValidateStruct(&i,
+	return validation.ValidateStruct(&i,
 		validation.Field(&i.LoginID, validation.Required, validation.Length(4, 16), customValidaton.NgWord),
 		validation.Field(&i.Password, validation.Required, validation.Length(8, 32)),
 	)
-	if err != nil {
-		return err
-	}
-	return nil
+}
+
+type ModifyNameInput struct {
+	Name string `json:"name"`
+}
+
+type ModifyAuthorityInput struct {
+	Authority int8 `json:"authority"`
+}
+
+type UserModifyOutput struct {
+	UserID    int    `json:"user_id"`
+	Name      string `json:"name"`
+	Authority int8   `json:"authority"`
+}
+
+func (i ModifyNameInput) Validate() error {
+	return validation.ValidateStruct(&i,
+		validation.Field(&i.Name, validation.Required, validation.Length(4, 16), customValidaton.NgWord),
+	)
+}
+
+func (i ModifyAuthorityInput) Validate() error {
+	return validation.ValidateStruct(&i,
+		validation.Field(&i.Authority, validation.Required, validation.Max(99)),
+	)
 }
