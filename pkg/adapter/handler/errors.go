566138536660229ab0ecd6f4367119b6d399dbc6
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-04-07 11:00:31 +0900

    [add]define http error

--- pkg/adapter/handler/errors.go
+++ pkg/adapter/handler/errors.go
@@ -3,10 +3,11 @@ package handler
 import "examples/pkg/adapter/infra"
 
 var (
-	ErrUnexpected   = NewHTTPError("エラー", "不明なエラー")
-	ErrPathNotExist = NewHTTPError("アクセスエラー", "アクセスパスが存在しません")
-	ErrValidParam   = NewHTTPError("入力値エラー", "入力された値が不正です")
-	ErrFailedSignin = NewHTTPError("サインイン失敗", "ログインIDまたはパスワードが間違っています")
+	ErrUnexpected       = NewHTTPError("エラー", "不明なエラー")
+	ErrPathNotExist     = NewHTTPError("アクセスエラー", "アクセスパスが存在しません")
+	ErrValidParam       = NewHTTPError("入力値エラー", "入力された値が不正です")
+	ErrFailedSignin     = NewHTTPError("サインイン失敗", "ログインIDまたはパスワードが間違っています")
+	HTTPErrUnauthorized = NewHTTPError("セッションエラー", "セッションが切れました。\n再度サインインしてください。")
 )
 
 func NewHTTPError(title, body string) *infra.HTTPError {
